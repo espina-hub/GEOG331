81290ff2cb04c2633929d4e3e2a1bc72d2b5ea0a
espina <espina@colgate.edu>
2025-09-04 11:09:01 -0400

    performed mathematical operations using vectos that have at least 4 numbs and variable names

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,20 @@
 print("Hello World")
+6^6
+234+8
+15-23
+a <- 6234
+a/3
+a*5
+##make a vector of numbers using c(...)
+b <- c(2395,82,2947)
+##divide all numbers by 2
+b/2
+#create new vector of numbers, run 3 calculations that use vectors
+#with at least 4 numbers and a variable name
+c <- c(17,33,901,88)
+d <- c(23, 77,90, 2)
+#all numbs divide by 4
+c/4
+#all numbs multiply by 3
+c*3
+d/2
