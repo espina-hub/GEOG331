a6ca40422204679086bc42f0056a6043fd3472e1
espina <espina@colgate.edu>
2025-09-11 11:07:05 -0400

    saving progress from class

---
+++ Activity2.R
@@ -0,0 +1,33 @@
+#initial code from assignment for visualization
+#make a vector of tree heights in meters
+heights <- c(30,41,20,22)
+#convert to cm
+heights_cm <- heights*100
+heights_cm
+#look at the first tree height
+heights[1]
+#get more info on the matrix function
+help(matrix)
+#example matrices
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+Mat
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+#byrow determines how the numbers fill in
+Mat.bycol
+#subset the matrix to look at row 1, column2
+Mat.bycol[1,2]
+#look at all values in row 1
+#opening in square bracket (blank before bracket) refers to all items
+Mat.bycol[1,]
+#look at all values in column 2
+Mat.bycol[,2]
+
+#read in weather station file from your data folder
+#copy file path requires me to add an additional \
+datW <- read.csv("Z:\\espina\\Data for Class\\noaa_weather\\2011124.csv")
+#get more information about the dataframe
+str(datW)
