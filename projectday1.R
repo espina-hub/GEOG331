866229530803a9be0c28a46c7d236cea9c54f1d5
espina-hub <espina@colgate.edu>
2025-11-13 11:10:09 -0500

    lol its doing nothing now, try again later

--- projectday1.R
+++ projectday1.R
@@ -1,6 +1,4 @@
-##Beginning Final Project!! Work day 1 (11/6)
-#install.packages(c("terra", "gdalUtilities")
-#install.packages("MODISTools")
+##Beginning Final Project!! Work day 1 (11/6) and 2 (11/13)
 
 library(terra)
 
@@ -9,13 +7,30 @@ file <- "Z:\\espina\\Data for Class\\final project data\\MOD21A1N.A2000173.h08v0
 
 #subsets
 data <- rast("Z:\\espina\\Data for Class\\final project data\\MOD21A1N.A2000173.h08v05.061.2020046031144.hdf")
-names(data)
-
 lst <- data[[1]]
 
+#phoenix shapefile
+phx <- vect("Z:\\espina\\Data for Class\\final project data\\City_Parcels\\City_Parcels.shp")
+#project phoenix shapefile into the modis
+
+phxphx_modis <- project(phx, crs(lst)) 
+
+#extract phx from modis tile
+values_phx <- extract(lst, phx_modis)
+values_phx
 
 
-#reproject to WGS84 so i can use lat/long coordinates
-ndvi_wgs84 <- terra::project(ndvi,"EPSG:4326")
-extent_phoenix <- ext(-113, -111, 32.5, 34.5)
+#convert temperature to celsius -- supposedly just raw numbers
+value_phx_k <- value_phx[[2]] * .02
+value_phx_k
 
+lst_c_phx <- lst_k_phx - 273.15
+lst_c_phx
+
+plot(lst_c_phx, main = "Phoenix Nighttime LST (C)")
+
+hist(values(lst_c_phx), 
+     main = "Distribution of Nighttime LST - Phoenix",
+     xlab = "Temperature (C)",
+     breaks = 50)
+quantile (values(lst_c_phx), probs = c(0.01, 0.5, 0.99), na.rm = TRUE)
