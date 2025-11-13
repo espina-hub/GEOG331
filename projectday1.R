33f53273a929624aacae10d732cfd7c9c56434af
espina-hub <espina@colgate.edu>
2025-11-13 10:09:55 -0500

    idk man

--- projectday1.R
+++ projectday1.R
@@ -8,15 +8,14 @@ library(terra)
 file <- "Z:\\espina\\Data for Class\\final project data\\MOD21A1N.A2000173.h08v05.061.2020046031144.hdf"
 
 #subsets
-sds <- terra::sds("Z:\\espina\\Data for Class\\final project data\\MOD21A1N.A2000173.h08v05.061.2020046031144.hdf")
-sds
+data <- rast("Z:\\espina\\Data for Class\\final project data\\MOD21A1N.A2000173.h08v05.061.2020046031144.hdf")
+names(data)
 
-names(sds)
-ndvi <- sds[[1]]
+lst <- data[[1]]
+
+
 
 #reproject to WGS84 so i can use lat/long coordinates
 ndvi_wgs84 <- terra::project(ndvi,"EPSG:4326")
 extent_phoenix <- ext(-113, -111, 32.5, 34.5)
 
-ndvi_phoenix <- crop(ndvi_wgs84, extent_phoenix)
-plot(ndvi_phoenix)
